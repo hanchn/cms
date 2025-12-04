4fe4574b38f6f369472fb7590f8c40ccb6273d67
yuanjing <jing.yuan@kapeixi.com>
2025-12-04 13:27:21 +0800

    .

--- src/views/MaterialFloorDetail/components/Editor.vue
+++ src/views/MaterialFloorDetail/components/Editor.vue
@@ -1,12 +1,30 @@
 <template>
   <div class="editor">
-    <a-textarea v-model:value="ctx.state.content" :rows="20" placeholder="在此编写楼层模板" />
+    <a-tabs v-model:activeKey="tab">
+      <a-tab-pane key="html" tab="HTML">
+        <CodeEditor v-model="ctx.state.html" language="html" height="260px" />
+      </a-tab-pane>
+      <a-tab-pane key="css" tab="CSS">
+        <CodeEditor v-model="ctx.state.css" language="css" height="260px" />
+      </a-tab-pane>
+      <a-tab-pane key="js" tab="JS">
+        <CodeEditor v-model="ctx.state.js" language="javascript" height="260px" />
+      </a-tab-pane>
+    </a-tabs>
   </div>
 </template>
 
 <script setup>
-import { inject } from 'vue'
+import { inject, ref, watch } from 'vue'
+import CodeEditor from '@/components/CodeEditor.vue'
 const ctx = inject('floorTemplateDetail')
+const tab = ref('html')
+function assemble(){
+  const css = ctx.state.css ? `<style>${ctx.state.css}</style>` : ''
+  const js = ctx.state.js ? `<script>${ctx.state.js}</script>` : ''
+  ctx.state.content = `${css}${ctx.state.html || ''}${js}`
+}
+watch(() => [ctx.state.html, ctx.state.css, ctx.state.js], assemble, { deep: true })
 </script>
 
 <style scoped>
